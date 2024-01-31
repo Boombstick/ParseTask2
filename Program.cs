43344742967c23817ad5e3fb8dc36aab3a06bc6c
Boombstick <BoombStick.Devel@gmail.com>
2024-01-31 12:33:11 +0400

    Task

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ParsingTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 100, i =>
+            {
+                if (i % 2 == 0)
+                    Server.AddToCount(2);
+                else
+                    Console.WriteLine(Server.GetCount());
+            });
+            Console.WriteLine(Server.GetCount());
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public static class Server
+    {
+        static ReaderWriterLockSlim  locker = new ReaderWriterLockSlim();
+        private static int count;
+        public static int GetCount()
+        {
+            locker.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally { locker.ExitReadLock(); }
+        }
+        public static void AddToCount(int value)
+        {
+            locker.EnterWriteLock();
+            Interlocked.Add(ref count,value);
+            locker.ExitWriteLock();
+        }
+    }
+}
